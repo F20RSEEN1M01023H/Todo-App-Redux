1bff7ea6757b98caf6789c650aeffa0a0c557a01
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-11-27 21:05:13 +0500

    Hamma is here

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-center font-bold text-5xl text-red-800">
+        Hammad is here
+      </h2>
+    </div>
+  );
+};
+
+export default App;

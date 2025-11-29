3976e9b833ad3aba99b55705aa1a55faa605b06d
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-11-29 21:34:39 +0500

    Hammad is here

--- src/store/actions/todo.jsx
+++ src/store/actions/todo.jsx
@@ -3,10 +3,38 @@ export const TOGGLE_TODO = "TOGGLE_TODO";
 export const EDIT_TODO = "EDIT_TODO";
 export const DELETE_TODO = "DELETE_TODO";
 
-export const addTodo = (text) => ({ type: ADD_TODO, payload: { text } });
+// sub tasks
+export const ADD_SUBTASK = "ADD_SUBTASK";
+export const TOGGLE_SUBTASK = "TOGGLE_SUBTASK";
+export const EDIT_SUBTASK = "EDIT_SUBTASK";
+export const DELETE_SUBTASK = "DELETE_SUBTASK";
+
+export const addTodo = (text, dueDate = null, subTasks = []) => ({
+  type: ADD_TODO,
+  payload: { text, dueDate, subTasks },
+});
 export const toggleTodo = (id) => ({ type: TOGGLE_TODO, payload: { id } });
 export const editTodo = (id, text) => ({
   type: EDIT_TODO,
   payload: { text, id },
 });
 export const deleteTodo = (id) => ({ type: DELETE_TODO, payload: { id } });
+
+// subtasks notes
+
+export const addSubTask = (todoId, text) => ({
+  type: ADD_SUBTASK,
+  payload: { todoId, text },
+});
+export const toggleSubTask = (todoId, subtaskId) => ({
+  type: TOGGLE_SUBTASK,
+  payload: { todoId, subtaskId },
+});
+export const editSubTask = (todoId, subtaskId, text) => ({
+  type: EDIT_SUBTASK,
+  payload: { todoId, subtaskId, text },
+});
+export const deleteSubTask = (todoId, subtaskId) => ({
+  type: DELETE_SUBTASK,
+  payload: { todoId, subtaskId },
+});
--- src/store/reducers/todo.jsx
+++ src/store/reducers/todo.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import { ADD_TODO, EDIT_TODO, TOGGLE_TODO, DELETE_TODO } from "../actions/todo";
+import {
+  ADD_TODO,
+  EDIT_TODO,
+  TOGGLE_TODO,
+  DELETE_TODO,
+  ADD_SUBTASK,
+  TOGGLE_SUBTASK,
+  DELETE_SUBTASK,
+  EDIT_SUBTASK,
+} from "../actions/todo";
 const initialState = [];
+
+function nextSubtaskId(subtasks) {
+  // simple id generator per todo
+  if (!subtasks || subtasks.length === 0) return 1;
+  return Math.max(...subtasks.map((s) => s.id)) + 1;
+}
 
 const todo = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO: {
-      const { text } = action.payload;
-      const newTodo = { id: Date.now(), text, completed: false };
+      const { text, dueDate = null, subtasks = [] } = action.payload;
+      const newTodo = {
+        id: Date.now(),
+        text,
+        completed: false,
+        dueDate: dueDate || null,
+        subtasks: subtasks.map((t, i) => ({
+          id: i + 1,
+          text: t.text,
+          completed: false,
+        })),
+      };
       return [...state, newTodo];
     }
     case TOGGLE_TODO: {
@@ -16,12 +41,33 @@ const todo = (state = initialState, action) => {
       );
     }
     case EDIT_TODO: {
-      const { id, text } = action.payload;
-      return state.map((t) => (t.id === id ? { ...t, text } : t));
+      const { id, text, dueDate } = action.payload;
+      return state.map((t) =>
+        t.id === id ? { ...t, text, dueDate: dueDate || null } : t
+      );
     }
     case DELETE_TODO: {
       const { id } = action.payload;
       return state.filter((t) => t.id !== id);
+    }
+
+    case ADD_SUBTASK: {
+      const { text, todoId } = action.payload;
+      return state.map((todo) => {
+        if (todo.id !== todoId) return todo;
+        const newId = nextSubtaskId(todo.subtasks);
+        return {
+          ...todo,
+          subtasks: [
+            ...(todo.subtasks || []),
+            {
+              id: newId,
+              text,
+              completed: false,
+            },
+          ],
+        };
+      });
     }
 
     default:
